e26c029b6f8c35ea80e77ae86098bd96b38e158a
CharlesBoboya <charlesotwari12@gmail.com>
2023-11-03 09:08:56 +0300

    commit this

---
+++ process_form.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    $to = "charlesotwari12@gmail.com"; // Replace with the recipient's email address
+    $subject = "New Contact Form Submission from $name";
+    $headers = "From: $email";
+
+    // Send email
+    mail($to, $subject, $message, $headers);
+
+    // Redirect back to the form after submission
+    header("Location: index.html");
+    exit;
+}
+?>
